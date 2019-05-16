5a78815ebc444e08f089bdb4cc6755ceceb45bd1
BlueAlien99 <45181394+BlueAlien99@users.noreply.github.com>
2019-05-16 23:01:03 +0200

    getPermutations

--- include/Utilities.h
+++ include/Utilities.h
@@ -2,10 +2,13 @@
 #define UTILITIES_H
 
 #include <cstdlib>
+#include <string>
+#include <set>
 
 class Utilities{
 public:
 	static uint get_uint();
+	static void getPermutations(std::string str, std::set<std::string> *perms, uint lock = 0);
 };
 
 #endif
--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -2,8 +2,11 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <set>
 
 #define INT_MAX_DIGITS 9
+#define STR_MAX_LENGTH 11
 
 using namespace std;
 
@@ -25,4 +28,20 @@ uint Utilities::get_uint(){
 		num = 0;
 	}
 	return num;
+}
+
+void Utilities::getPermutations(string str, set<string> *perms, uint lock){
+	if(str.size() > STR_MAX_LENGTH){
+		string thr = "String longer than " + to_string(STR_MAX_LENGTH) + '!';
+		throw thr;
+	}
+	if(lock >= str.size()-1){
+		perms->insert(str);
+		return;
+	}
+	for(uint i = lock; i < str.size(); ++i){
+		string stri = str;
+		iter_swap(stri.begin()+lock, stri.begin()+i);
+		getPermutations(stri, perms, lock+1);
+	}
 }
